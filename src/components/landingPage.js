53be37fd030fa6ecae93978e7ff9357b08643511
Cameron Sellers <cameron.sellers07@gmail.com>
2020-07-11 18:00:07 -0600

    changing dot sizes on landing page

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -37,31 +37,31 @@ const landingPage = () => {
       <div className="reviewContainer">
         <Carousel breakPoints={breakPoints}>
           <Item>
-            These guys are great! Knowledgeable, friendly, reasonable, and they
-            work hard to make sure your needs are met! I highly recommend them!
+            "These guys are great! Knowledgeable, friendly, reasonable, and they
+            work hard to make sure your needs are met! I highly recommend them!"
             -James Moody
           </Item>
           <Item>
-            Super friendly, fast, and great cases too. I took my iPhone 8 there
+            "Super friendly, fast, and great cases too. I took my iPhone 8 there
             with a terrible cracked screen and picked out a new case. An hour
-            later I had my phone with the case on it. Awesome!! -Adam Beaty
+            later I had my phone with the case on it. Awesome!!" -Adam Beaty
           </Item>
           <Item>
-            They are honest & capable. And their products are very reasonably
-            priced…not like a typical cellular store. - Teresa Draughn
+            "They are honest & capable. And their products are very reasonably
+            priced…not like a typical cellular store." - Teresa Draughn
           </Item>
           <Item>
-            Always so helpful and friendly! I recommend them to everyone looking
-            for phone plans & repairs! - Kayla BethAnn Brown Voiles
+            "Always so helpful and friendly! I recommend them to everyone
+            looking for phone plans & repairs!" - Kayla BethAnn Brown Voiles
           </Item>
           <Item>
-            Locally owned and operated. Great prices, repair phones, Great
-            Customer service - Raymond Phillips
+            "Locally owned and operated. Great prices, repair phones, Great
+            Customer service." - Raymond Phillips
           </Item>
           <Item>
-            They replaced my battery in my I phone 6 today very reasonable fast
+            "They replaced my battery in my I phone 6 today very reasonable fast
             service very friendly and courteous if you need your I phone worked
-            on please pay them a visit. - Jo Ann Brewington
+            on please pay them a visit." - Jo Ann Brewington
           </Item>
         </Carousel>
       </div>

42ef02adfd61722da42269f69e7b71bef2e46d09
Cameron Sellers <cameron.sellers07@gmail.com>
2020-07-15 13:47:58 -0600

    add icons and locations to front page

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Carousel from "react-elastic-carousel";
 import Item from "./carousel";
-import { Card } from "reactstrap";
+import { Card, Toast, ToastBody, ToastHeader } from "reactstrap";
 
 import "./landingPage.css";
 
@@ -19,6 +19,20 @@ const landingPage = () => {
         src="https://res.cloudinary.com/drcgo7zqn/image/upload/v1594319332/Phone%20Smart/pr-media-iuU2aZdzp_M-unsplash_c6xvww.jpg"
         id="heroImg"
       />
+      <div className="services-icons-container">
+        <Card className="repair-card">
+          <i class="fas fa-toolbox"></i>
+          <h4>Device Repair</h4>
+        </Card>
+        <Card className="mobile-plans-card">
+          <i class="fas fa-mobile-alt"></i>
+          <h4>Mobile Plans</h4>
+        </Card>
+        <Card className="buy-sell-trade-card">
+          <i class="far fa-money-bill-alt"></i>
+          <h4> Buy, Sell & Trade</h4>
+        </Card>
+      </div>
       <Card className="tagline-card">
         <p className="tagline">
           We specialize in fast, affordable repairs for electronic devices! We
@@ -47,6 +61,20 @@ const landingPage = () => {
           <span>Contact Us</span>
         </h1>
       </Card>
+      <div className="toast-container">
+        <Toast>
+          <ToastHeader>Cookville, TN</ToastHeader>
+          <ToastBody>
+            728 S Jefferson Ave, Suite 15 Cookeville, Tennessee 38501
+          </ToastBody>
+        </Toast>
+        <Toast>
+          <ToastHeader>Jamestown, TN</ToastHeader>
+          <ToastBody>
+            205 South Main Street Jamestown, Tennessee 38556
+          </ToastBody>
+        </Toast>
+      </div>
       <div className="reviewContainer">
         <Carousel breakPoints={breakPoints}>
           <Item>

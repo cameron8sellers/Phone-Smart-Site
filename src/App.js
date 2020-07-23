509fb91246c0cdeca0a03e91381b1c44e9ef9a6a
Cameron Sellers <cameron.sellers07@gmail.com>
2020-07-23 14:43:52 -0600

    plans page and route changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import contactPage from "./components/contact";
 import Menu from "./slideMenu";
 import landingPage from "./components/landingPage";
+import mobilePlansPage from "./components/mobilePlans";
 
 import "./App.css";
 import { Route } from "react-router-dom";
@@ -11,17 +12,20 @@ function App() {
     <div className="App">
       <nav className="nav">
         <div className="logoContainer">
-          <img
-            alt=""
-            src="https://res.cloudinary.com/drcgo7zqn/image/upload/v1594321677/Phone%20Smart/imageedit_5_3793295319_eu3y6r.png"
-            className="logo-mobile"
-          />
+          <a href="/">
+            <img
+              alt=""
+              src="https://res.cloudinary.com/drcgo7zqn/image/upload/v1594321677/Phone%20Smart/imageedit_5_3793295319_eu3y6r.png"
+              className="logo-mobile"
+            />
+          </a>
         </div>
         <Menu />
       </nav>
 
       <Route exact path="/" component={landingPage} />
       <Route path="/contact" component={contactPage} />
+      <Route path="/plans" component={mobilePlansPage} />
 
       <footer>
         <p className="copyrightDevName">© The Spare Room</p>
